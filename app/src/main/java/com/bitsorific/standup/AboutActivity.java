5a112c478ae21ab9d23473467926a6fc7b8bffa6
nischalshrestha <bitsorific@gmail.com>
2016-02-09 19:58:38 -0500

    Adding an About activity

---
+++ app/src/main/java/com/bitsorific/standup/AboutActivity.java
@@ -0,0 +1,19 @@
+package com.bitsorific.standup;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by nischal on 2/9/16.
+ */
+public class AboutActivity extends AppCompatActivity {
+
+//    private TextView aboutText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceBudle){
+        super.onCreate(savedInstanceBudle);
+        setContentView(R.layout.activity_about);
+    }
+
+}
--- app/src/main/java/com/bitsorific/standup/MainActivity.java
+++ app/src/main/java/com/bitsorific/standup/MainActivity.java
@@ -1,12 +1,15 @@
 package com.bitsorific.standup;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
 import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -167,6 +170,28 @@ public class MainActivity extends AppCompatActivity {
             timerUnitView.setTextColor(standColor);
         }
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_activity_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.about) {
+            startActivity(new Intent(this, AboutActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
